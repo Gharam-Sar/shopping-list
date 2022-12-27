28ce0ba2a9ca2e5d023d9799f1b1741f67f94403
Gharam-Sar <sargharam@gmail.com>
2022-12-27 21:56:45 +0200

    items now are listed in the cart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import trash from "./imgs/trash.png";
 import cart from "./imgs/cart.png";
+import x from "./imgs/x.png";
 import pen from "./imgs/pen.png";
 import notebook from "./imgs/notebook.png";
 import eraser from "./imgs/eraser.png";
@@ -15,58 +16,112 @@ import backpack from "./imgs/backpack.png";
 import bag from "./imgs/bag.png";
 import colors from "./imgs/colors.png";
 
-let itemsOnDisplay =[{ id: 0, name: "Pen", img: pen,price :5 },{ id: 1, name: "Notebook", img: notebook,price :10 },
-{ id: 2, name: "Pens Holder", img: holder,price :15 },{ id: 3, name: "Scissors", img: scissors,price :10 },
-{ id: 4, name: "Ruler", img: ruler,price :10 },{ id: 5, name: "Pencil", img: pencil,price :5 },
-{ id: 6, name: "Sharpener", img: sharpener,price :5 },{ id: 7, name: "Tape", img: tape,price :10 },
-{ id: 8, name: "Pens Bag", img: bag,price :15 },{ id: 9, name: "Backpack", img: backpack,price :20 },
-{ id: 10, name: "Eraser", img: eraser,price :5 },{ id: 11, name: "Coloring Pens", img: colors,price :15 }];
+let itemsOnDisplay = [
+  { id: 0, name: "Pen", img: pen, price: 5 },
+  { id: 1, name: "Notebook", img: notebook, price: 10 },
+  { id: 2, name: "Pens Holder", img: holder, price: 15 },
+  { id: 3, name: "Scissors", img: scissors, price: 10 },
+  { id: 4, name: "Ruler", img: ruler, price: 10 },
+  { id: 5, name: "Pencil", img: pencil, price: 5 },
+  { id: 6, name: "Sharpener", img: sharpener, price: 5 },
+  { id: 7, name: "Tape", img: tape, price: 10 },
+  { id: 8, name: "Pens Bag", img: bag, price: 15 },
+  { id: 9, name: "Backpack", img: backpack, price: 20 },
+  { id: 10, name: "Eraser", img: eraser, price: 5 },
+  { id: 11, name: "Coloring Pens", img: colors, price: 15 },
+];
 
-let cartItems=[];
-let itemColor=['#a0bcc2','#ddbcce','#c4dfaa','#d7c0ae','#e3bec6','#d0fca5','#dcb89c','#ffeccb','#fffab8','#F6AE99','#C9E4C5','#c8bde1'];
+let cartItems = [];
+let itemColor = [
+  "#a0bcc2",
+  "#ddbcce",
+  "#c4dfaa",
+  "#d7c0ae",
+  "#e3bec6",
+  "#d0fca5",
+  "#dcb89c",
+  "#ffeccb",
+  "#fffab8",
+  "#F6AE99",
+  "#C9E4C5",
+  "#c8bde1",
+];
 function Item({ item }) {
-
   return (
-  
-<div className="myDiv" key={item.id}   style={{ backgroundColor: itemColor[item.id] }}>
-<h2>Item Name: {item.name}</h2>
-          <img className="itemImg"
-          src={item.img}
-        ></img>
-<p>Price: {item.price}</p>
-<button className="addButton" 
- onClick={() => {
-  let found=false;
-  if(cartItems.length==0) {cartItems.push({id: item.id, amount: 1}); found=true;}
-  else{
-   for (let i = 0; i < cartItems.length; i++)
-  {
-    console.log("lllll");
- 
-if(item.id==cartItems[i].id){
-  let amount=cartItems[i].amount;
-  cartItems[i].amount=amount+1;
-  found=true;
-  break;
-}
+    <div
+      className="myDiv"
+      key={item.id}
+      style={{ backgroundColor: itemColor[item.id] }}
+    >
+      <h2>Item Name: {item.name}</h2>
+      <img className="itemImg" src={item.img}></img>
+      <p>Price: {item.price}</p>
+      <button
+        className="addButton"
+        onClick={() => {
+          let found = false;
+          if (cartItems.length == 0) {
+            cartItems.push({
+              id: item.id,
+              name: item.name,
+              price: item.price,
+              img: item.img,
+              amount: 1,
+            });
+            found = true;
+          } else {
+            for (let i = 0; i < cartItems.length; i++) {
+              console.log("lllll");
 
-  }
-  if(!found) cartItems.push({id: item.id, amount: 1});
+              if (item.id == cartItems[i].id) {
+                let amount = cartItems[i].amount;
+                cartItems[i].amount = amount + 1;
+                found = true;
+                break;
+              }
+            }
+            if (!found)
+              cartItems.push({
+                id: item.id,
+                name: item.name,
+                price: item.price,
+                img: item.img,
+                amount: 1,
+              });
+          }
 
-  }
-  
-console.log(cartItems);
-
-}}>
-                Add to Cart
-              </button>
-        </div>
-
+          console.log(cartItems);
+        }}
+      >
+        Add to Cart
+      </button>
+    </div>
   );
 }
+function CartItem({ item }) {
+  return (
+    <div className="cartDiv" key={item.id}>
+      <p>
+        {" "}
+        Item Name: {item.name} &emsp;&emsp;Price: {item.price}{" "}
+        &emsp;&emsp;Amount={item.amount}{" "}
+      </p>
+      <p> Total amount: {item.amount * item.price} </p>
 
-function Items({  filterText, toggle }) {
-
+      <br></br>
+    </div>
+  );
+}
+function CartItems() {
+  return (
+    <div className="cartItems">
+      {cartItems.map((item) => {
+        return <CartItem item={item} />;
+      })}
+    </div>
+  );
+}
+function Items() {
   return (
     <div>
       <div className="items">
@@ -78,22 +133,23 @@ function Items({  filterText, toggle }) {
   );
 }
 export default function MyApp() {
+  const [toggle, settoggle] = useState(false);
+
   return (
     <div>
       <div className="title">
-        <h1>Happy Shopping </h1><img className="cartImg"
-          src={cart}
+        <h1>Happy Shopping </h1>
+        <img
+          className="cartImg"
+          onClick={() => {
+            settoggle(!toggle);
+          }}
+          src={toggle ? x : cart}
         ></img>
       </div>
       <hr></hr>
-      <div className="componant">
-      <Items
-       
-      />
-      </div>
+      <div className="componant">{toggle ? <CartItems /> : <Items />}</div>
       <hr></hr>
-
-     
     </div>
   );
 }
